e2639b67b01f4fac9c5cf6dd132b65f16e76fdf7
YH <azss0470@gmail.com>
2020-05-29 00:35:47 +0900

    0. 도시가 100개라 배열문제로 인해 틀린 줄 알았다. 1. 동적배열할당이 문제가 아님. 2. 문제를 보면 i에서 j로 못가는 경우 0으로 출력. 3. 마지막 출력 시 INF를 0으로 바꿔주었다. 결론 : 문제를 잘 읽자!

--- 2020BaekJoon/FloydWarshall_11404.cpp
+++ 2020BaekJoon/FloydWarshall_11404.cpp
@@ -7,58 +7,62 @@
 //  [도시와 버스]
 
 #include <iostream>
+#include <algorithm>
 using namespace std;
 
 
 int n = 0; // (1<=n<=100) 도시의 개수
 int m = 0; // (1<=m<=100,000) 버스의 개수
 int INF = 10000000; // 한번에 갈 수 없는 경우
+int D[101][101];
+
+// 배열 초기화 함수
+void init(){
+    for(int i=0;i<=n;i++){
+        for(int j=0;j<=n;j++){
+            D[i][j] = i == j ? 0 : INF; // i == j 이면 0 아니면 INF 입력
+        }
+    }
+}
+
+// 플로이드 함수
+void floyd(){
+    for(int k=1;k<=n;k++){ // 거쳐서 가는 경우
+        for(int i=1;i<=n;i++){ // 시작점부터
+            for(int j=1;j<=n;j++){ // 끝점까지
+                D[i][j] = min(D[i][j], D[i][k] + D[k][j]); // 두 값중 더 작은 값을 입력
+            }
+        }
+    }
+}
+
 
 
 int main(){
     
-    
     cin >> n; // 첫째 줄에 도시의 개수를 입력
     cin >> m; // 둘째 줄에 버스의 개수를 입력
     
-    int **D; // 동적할당으로 최소비용을 저장할 2차원 배열 생성
-    D = new int*[n]; // 행 결정
-    for(int i=0;i<n;i++){ // 열 결정
-        D[i] = new int[n];
-    }
-    
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
-            if(i==j) D[i][j] = 0;
-            else D[i][j] = INF;
-        }
-    }
-    
+    init(); // 배열 초기화
     
     // 셋째줄부터는 버스의정보입력(시작도시번호, 도착도시번호, 한번타는데필요한비용)
-    int x=0,y=0,z=0; // 시작번호, 도착번호, 비용을 저장할 변수
+    int from=0,to=0,weight=0; // 시작번호, 도착번호, 비용을 저장할 변수
     for(int i=0 ; i<m ; i++){ // 버스의 개수만큼 입력
-        cin >> x >> y >> z; // 시작번호, 도착번호, 비용을 입력
-        if(D[x-1][y-1] >= z) D[x-1][y-1] = z; // 배열은 0~n-1까지 이므로 x,y에 -1해준다. 해당배열에 비용 입력
+        cin >> from >> to >> weight; // 시작번호, 도착번호, 비용을 입력
+        if(D[from][to] > weight) D[from][to] = weight;
+        
     }
-    
-    
-    for(int k=0;k<n;k++){
-        for(int i=0;i<n;i++){
-            for(int j=0;j<n;j++){
-                if(D[i][j] > D[i][k] + D[k][j]) D[i][j] = D[i][k] + D[k][j];
-            }
-        }
-    }
-    
-
-    for(int i=0;i<n;i++){
-        for(int j=0;j<n;j++){
+    // 플로이드
+    floyd();
+    // 출력부분
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=n;j++){
+            if(D[i][j] == INF) D[i][j] = 0; // i에서 j로 갈 수 없는 경우 그 자리를 0으로 입력
             cout << D[i][j] << " ";
         }
         cout << endl;
     }
     
-    
+
     return 0;
 }
