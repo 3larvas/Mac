76331b80f925e9c3080d808e4f6f88d3916b7694
OOHYEE <azss0470@gmail.com>
2020-05-28 21:15:54 +0900

    Initial Commit

---
+++ 2020BaekJoon/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  2020BaekJoon
+//
+//  Created by YeHwan Oh on 2020/05/28.
+//  Copyright © 2020 YeHwan Oh. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
