ee75c258728885f36ad135905497304e0af1756e
YH <azss0470@gmail.com>
2020-05-29 01:11:30 +0900

    생각.

---
+++ 2020BaekJoon/(냅색)평범한배낭_12865.cpp
@@ -0,0 +1,31 @@
+//
+//  (냅색)평범한배낭_12865.cpp
+//  2020BaekJoon
+//
+//  Created by YeHwan Oh on 2020/05/29.
+//  Copyright © 2020 YeHwan Oh. All rights reserved.
+//
+#include <iostream>
+using namespace std;
+
+int N=0; // 필요한 물건의 개수
+int W=0; // 물건의 무게
+int V=0; // 물건의 가치
+
+int K=0; // 배낭의 최대 무게
+
+
+
+int main(void){
+    
+    cin >> N >> K;
+    
+    for(int i=0; i<N; i++){
+        cin >> W >> V;
+    }
+    
+    // 2차원 배열을 통해 구현
+    
+    
+    return 0;
+}
